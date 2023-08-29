23d9c1cf22c46c96c979203e4b43370993021ac6
sunxiaoou <sun_xiaoou@qq.com>
2023-08-29 15:54:35 +0800

    add local mode

--- src/main/java/xo/mapreduce/WordCount.java
+++ src/main/java/xo/mapreduce/WordCount.java
@@ -1,5 +1,7 @@
 package xo.mapreduce;
 
+import xo.hdfs.Hdfs;
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
@@ -13,12 +15,15 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.security.UserGroupInformation;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class WordCount extends Configured implements Tool {
-    final static String input = "wordcount/input";
-    final static String output = "wordcount/output";
+    private static final Logger LOG = LoggerFactory.getLogger(WordCount.class);
 
     public static class MyMapper extends Mapper<LongWritable, Text, Text, LongWritable> {
         @Override
@@ -46,13 +51,14 @@ public class WordCount extends Configured implements Tool {
     }
 
     @Override
-    public int run(String[] strings) throws Exception {
+    public int run(String[] args) throws Exception {
         Job job = Job.getInstance(super.getConf(), "WordCount");
         job.setJarByClass(WordCount.class);
 
         job.setInputFormatClass(TextInputFormat.class);
 //        TextInputFormat.addInputPath(job, new Path("file:///" + base + "/input"));
-        TextInputFormat.addInputPath(job, new Path(input));
+//        TextInputFormat.addInputPath(job, new Path(input));
+        TextInputFormat.addInputPath(job, new Path(args[0]));
 
         job.setMapperClass(MyMapper.class);
         job.setMapOutputKeyClass(Text.class);
@@ -64,22 +70,71 @@ public class WordCount extends Configured implements Tool {
 
         job.setOutputFormatClass(TextOutputFormat.class);
 //        TextOutputFormat.setOutputPath(job, new Path("file:///" + base + "/output"));
-        TextOutputFormat.setOutputPath(job, new Path(output));
+//        TextOutputFormat.setOutputPath(job, new Path(output));
+        TextOutputFormat.setOutputPath(job, new Path(args[1]));
 
         boolean b = job.waitForCompletion(true);
         return b ? 0: 1;
     }
 
+    private static void delDir(String dir) throws IOException {
+        java.nio.file.Path path = Paths.get(dir);
+        if (Files.exists(path)) {
+            Files.walk(path)
+                    .sorted((p1, p2) -> -p1.compareTo(p2)) // Delete from bottom to top
+                    .forEach(p -> {
+                        try {
+                            Files.delete(p);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    });
+        }
+    }
+
+    private static String readFile(String file) throws IOException {
+        java.nio.file.Path path = Paths.get(file);
+        byte[] fileBytes = Files.readAllBytes(path);
+        return new String(fileBytes);
+    }
+
+    private static int mr() throws Exception {
+        String base = System.getProperty("user.dir");
+        String input = base + "/mapreduce/wordcount/input";
+        String output = base + "/mapreduce/wordcount/output";
+        delDir(output);
+        Configuration conf = new Configuration();
+        int rc = ToolRunner.run(conf, new WordCount(), new String[]{"file:///" + input, "file:///" + output});
+        LOG.info(readFile(output + "/part-r-00000"));
+        return rc;
+    }
+
+    // Cannot run in IDE
     // $ mvn clean package
     // $ hadoop jar target/bigdata-1.0-SNAPSHOT.jar xo.mapreduce.WordCount
-    public static void main(String[] args) throws Exception {
-        UserGroupInformation ugi = UserGroupInformation.createRemoteUser("sunxo");
+    private static int mr(String host, int port, String user) throws Exception {
+        String input = "wordcount/input";
+        String output = "wordcount/output";
+
+        UserGroupInformation ugi = UserGroupInformation.createRemoteUser(user);
         UserGroupInformation.setLoginUser(ugi);
         Configuration conf = new Configuration();
-        conf.set("fs.defaultFS", "hdfs://ubuntu:8020");
-        conf.set("yarn.resourcemanager.hostname", "ubuntu");
+        conf.set("fs.defaultFS", String.format("hdfs://%s:%d", host, port));
+        conf.set("yarn.resourcemanager.hostname", host);
 
-        int rc = ToolRunner.run(conf, new WordCount(), args);
-        System.exit(rc);
+        Hdfs hdfs = new Hdfs(conf);
+        hdfs.delFile(output);
+        int rc = ToolRunner.run(conf, new WordCount(), new String[]{input, output});
+        LOG.info(hdfs.readFile(output + "/part-r-00000"));
+        hdfs.close();
+        return rc;
+    }
+
+    public static void main(String[] args) throws Exception {
+        if (args.length > 0) {
+            LOG.info(String.valueOf(mr()));
+        } else {
+            LOG.info(String.valueOf(mr("ubuntu", 8020, "sunxo")));
+        }
     }
 }
