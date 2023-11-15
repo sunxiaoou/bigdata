451e981009601c06173d60fb9dea93656c41d0d9
sunxiaoou <sun_xiaoou@qq.com>
2023-11-15 22:05:52 +0800

    add deserializer

--- src/main/java/xo/fastjson/JsonUtil.java
+++ src/main/java/xo/fastjson/JsonUtil.java
@@ -51,6 +51,8 @@ public class JsonUtil {
             return Arrays.copyOfRange(arr, offset, offset + length);
         }
 
+        Cell4Json() {}
+
         Cell4Json(Cell cell) {
             this.row = new String(extractBytes(cell.getRowArray(), cell.getRowOffset(), cell.getRowLength()));
             this.family = new String(extractBytes(cell.getFamilyArray(), cell.getFamilyOffset(),
@@ -61,6 +63,11 @@ public class JsonUtil {
             this.timestamp = new Date(cell.getTimestamp());
             this.value = extractBytes(cell.getValueArray(), cell.getValueOffset(), cell.getValueLength());
         }
+
+        Cell getCell() {
+            return new KeyValue(Bytes.toBytes(row), Bytes.toBytes(family), Bytes.toBytes(qualifier),
+                    timestamp.getTime(), value);
+        }
     }
 
     @Data
@@ -69,10 +76,20 @@ public class JsonUtil {
         private List<Cell4Json> cells;
         private Set<String> families;
 
+        Edit4Json() {}
+
         Edit4Json(WALEdit edit) {
             this.replay = edit.isReplay();
             this.cells = edit.getCells().stream().map(Cell4Json::new).collect(Collectors.toList());
             this.families = edit.getFamilies().stream().map(String::new).collect(Collectors.toSet());
+        }
+
+        WALEdit getEdit() {
+            WALEdit edit = new WALEdit(cells.size(), replay);
+            for (Cell4Json cell4Json : cells) {
+                edit.add(cell4Json.getCell());
+            }
+            return edit;
         }
     }
 
@@ -81,9 +98,15 @@ public class JsonUtil {
         private Edit4Json edit;
         private Key4Json key;
 
+        Entry4Json() {}
+
         Entry4Json(WAL.Entry entry) {
             this.edit = new Edit4Json(entry.getEdit());
             this.key = new Key4Json(entry.getKey());
+        }
+
+        WAL.Entry getEntry() {
+            return new WAL.Entry((WALKeyImpl) key.getKey(), edit.getEdit());
         }
     }
 
@@ -93,13 +116,19 @@ public class JsonUtil {
     }
 
     public static WALKey json2Key(String json) {
-        Key4Json key4Json = JSON.parseObject(json, JsonUtil.Key4Json.class);
+        Key4Json key4Json = JSON.parseObject(json, Key4Json.class);
         return key4Json.getKey();
     }
 
     public static String cells2Json(List<Cell> cells) {
         List<Cell4Json> list = cells.stream().map(Cell4Json::new).collect(Collectors.toList());
-        return JSON.toJSONString(list, SerializerFeature.WriteDateUseDateFormat);
+//        return JSON.toJSONString(list, SerializerFeature.WriteDateUseDateFormat);
+        return JSON.toJSONString(list);
+    }
+
+    public static List<Cell> json2Cells(String json) {
+        List<Cell4Json> list = JSON.parseArray(json, Cell4Json.class);
+        return list.stream().map(Cell4Json::getCell).collect(Collectors.toList());
     }
 
     public static String edit2Json(WALEdit edit) {
@@ -107,33 +136,56 @@ public class JsonUtil {
         return JSON.toJSONString(edit4Json, SerializerFeature.WriteDateUseDateFormat);
     }
 
+    public static WALEdit json2Edit(String json) {
+        Edit4Json edit4Json = JSON.parseObject(json, Edit4Json.class);
+        return edit4Json.getEdit();
+    }
+
     public static String entry2Json(WAL.Entry entry) {
         Entry4Json entry4Json = new Entry4Json(entry);
         return JSON.toJSONString(entry4Json, SerializerFeature.WriteDateUseDateFormat);
     }
 
+    public static WAL.Entry json2Entry(String json) {
+        Entry4Json entry4Json = JSON.parseObject(json, Entry4Json.class);
+        return entry4Json.getEntry();
+    }
+
     public static void main(String[] args) {
+        long current = System.currentTimeMillis();
         WALKeyImpl key = new WALKeyImpl(Bytes.toBytes("encode_region_name"), TableName.valueOf("manga:fruit"),
-                42, System.currentTimeMillis(), HConstants.DEFAULT_CLUSTER_ID);
+                42, current, HConstants.DEFAULT_CLUSTER_ID);
         List<Cell> cells = new ArrayList<>();
         cells.add(new KeyValue(Bytes.toBytes("107"), Bytes.toBytes("cf"), Bytes.toBytes("name"),
-                Bytes.toBytes("🍐")));
+                current, Bytes.toBytes("🍐")));
         cells.add(new KeyValue(Bytes.toBytes("107"), Bytes.toBytes("cf"), Bytes.toBytes("price"),
-                Bytes.toBytes(115)));
+                current, Bytes.toBytes(115)));
         WALEdit edit = new WALEdit(cells.size(), false);
         for (Cell cell : cells) {
             edit.add(cell);
         }
         WAL.Entry entry = new WAL.Entry(key, edit);
 
-        System.out.println(key);
-        String json = key2Json(key);
-        System.out.println(json);
-        System.out.println(json2Key(json));
+        String json;
 
-//        System.out.println(cells2Json(cells));
-//        System.out.println(edit2Json(edit));
+//        System.out.println(key);
+//        json = key2Json(key);
+//        System.out.println(json);
+//        System.out.println(json2Key(json));
+
+//        System.out.println(cells);
+//        json = cells2Json(cells);
+//        System.out.println(json);
+//        System.out.println(json2Cells(json));
+
+//        System.out.println(edit);
+//        json = edit2Json(edit);
+//        System.out.println(json);
+//        System.out.println(json2Edit(json));
+
         System.out.println(entry);
-//        System.out.println(entry2Json(entry));
+        json = entry2Json(entry);
+        System.out.println(json);
+        System.out.println(json2Entry(json));
     }
 }
