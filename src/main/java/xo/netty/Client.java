a2ce3d39591652d1986e4d58abdda5545da03851
sunxiaoou <sun_xiaoou@qq.com>
2024-05-12 16:27:10 +0800

    initial

---
+++ src/main/java/xo/netty/Client.java
@@ -0,0 +1,60 @@
+package xo.netty;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+import java.net.InetSocketAddress;
+
+
+public class Client {
+    private final String host;
+    private final int port;
+
+    public Client(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public void start()
+        throws Exception {
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(group)
+                .channel(NioSocketChannel.class)
+                .remoteAddress(new InetSocketAddress(host, port))
+                .handler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    public void initChannel(SocketChannel ch)
+                        throws Exception {
+                        ch.pipeline().addLast(new LoggingHandler(LogLevel.DEBUG));
+                        ch.pipeline().addLast(new CltHandler());
+                    }
+                });
+            ChannelFuture f = b.connect().sync();
+            f.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully().sync();
+        }
+    }
+
+    public static void main(String[] args)
+            throws Exception {
+        if (args.length != 2) {
+            System.err.println("Usage: " + Client.class.getSimpleName() + " <host> <port>");
+            return;
+        }
+
+        final String host = args[0];
+        final int port = Integer.parseInt(args[1]);
+        new Client(host, port).start();
+    }
+}
+
---
+++ src/main/java/xo/thread/Backup.java
@@ -0,0 +1,295 @@
+package xo.thread;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Consumer;
+
+
+class SnapshotThread implements Runnable {
+    private static final Logger LOG = LoggerFactory.getLogger(SnapshotThread.class);
+
+    private final int threadCount;
+    private final BlockingQueue<String> snapshotQueue;
+    private final BlockingQueue<String> exportQueue;
+    private final AtomicBoolean stopFlag;
+    private final Consumer<Boolean> stopFn;
+
+    public SnapshotThread(int threadCount,
+                          BlockingQueue<String> snapshotQueue,
+                          BlockingQueue<String> exportQueue,
+                          AtomicBoolean stopFlag,
+                          Consumer<Boolean> stopFn) {
+        this.threadCount = threadCount;
+        this.snapshotQueue = snapshotQueue;
+        this.exportQueue = exportQueue;
+        this.stopFlag = stopFlag;
+        this.stopFn = stopFn;
+    }
+
+    private String snapshot(String table) throws InterruptedException {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyMMdd");
+        String dateStr = sdf.format(new Date());
+        String snapshot = table + "_" + dateStr;
+        Thread.sleep(100);
+        LOG.info("Generated snapshot: " + snapshot);
+        return snapshot;
+    }
+
+    private boolean checkStopFlag() {
+        return stopFlag.get() || Thread.currentThread().isInterrupted();
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!checkStopFlag()) {
+                String table = snapshotQueue.poll();
+                if (table == null) {
+                    for (int i = 0; i < threadCount; i ++) {
+                        exportQueue.put("");
+                    }
+                    break;
+                }
+                String snapshot = snapshot(table);
+                exportQueue.put(snapshot);
+            }
+        } catch (Exception e) {
+            if (!stopFlag.get()) {
+                LOG.error("unexpected exception in snapshot thread");
+                stopFn.accept(false);
+            }
+        } finally {
+            LOG.info("snapshot thread exited");
+        }
+    }
+}
+
+class ExportThread implements Runnable {
+    private static final Logger LOG = LoggerFactory.getLogger(ExportThread.class);
+
+    private final BlockingQueue<String> exportQueue;
+    private final BlockingQueue<String> restoreQueue;
+    private final AtomicBoolean stopFlag;
+    private final Consumer<Boolean> stopFn;
+    private final CountDownLatch countDownLatch;
+//    private final JobStatusCounter statusCounter;
+//    private final RuleState ruleState;
+//    private final RuleConfig ruleConfig;
+
+    public ExportThread(BlockingQueue<String> exportQueue,
+                        BlockingQueue<String> restoreQueue,
+                        AtomicBoolean stopFlag,
+                        Consumer<Boolean> stopFn,
+                        CountDownLatch countDownLatch) {
+        this.exportQueue = exportQueue;
+        this.restoreQueue = restoreQueue;
+        this.stopFlag = stopFlag;
+        this.stopFn = stopFn;
+        this.countDownLatch = countDownLatch;
+    }
+
+    private void export(String snapshot) throws InterruptedException {
+        Thread.sleep(2000);
+        LOG.info("Export snapshot: " + snapshot);
+    }
+
+    private boolean checkStopFlag() {
+        return stopFlag.get() || Thread.currentThread().isInterrupted();
+    }
+
+    @Override
+    public void run() {
+        String mine = Thread.currentThread().getName();
+        try {
+            while (!checkStopFlag()) {
+                String snapshot = exportQueue.take();
+                if ("".equals(snapshot)) {
+                    break;
+                }
+                export(snapshot);
+                restoreQueue.put(snapshot);
+            }
+        } catch (Exception e) {
+            if (!stopFlag.get()) {
+                LOG.error("unexpected exception in export thread {}", mine);
+                stopFn.accept(false);
+            }
+        } finally {
+            countDownLatch.countDown();
+            LOG.info("export thread {} exited", mine);
+        }
+    }
+}
+
+class ExportThreadManager {
+    private static final Logger LOG = LoggerFactory.getLogger(ExportThreadManager.class);
+
+    private final int threadCount;
+    private final BlockingQueue<String> exportQueue;
+    private final BlockingQueue<String> restoreQueue;
+//    private final RuleConfig ruleConfig;
+//    private final RuleState ruleState;
+//    private final JobStatusCounter jobStatusCounter;
+    private final AtomicBoolean stopFlag;
+    private final Consumer<Boolean> stopFn;
+    private final CountDownLatch countDownLatch;
+    private final ExecutorService pool;
+
+    public ExportThreadManager(int threadCount,
+                               BlockingQueue<String> exportQueue,
+                               BlockingQueue<String> restoreQueue,
+                               AtomicBoolean stopFlag,
+                               Consumer<Boolean> stopFn) {
+        this.threadCount = threadCount;
+        this.exportQueue = exportQueue;
+        this.restoreQueue = restoreQueue;
+//        this.ruleState = ruleState;
+//        this.jobStatusCounter = jobStatusCounter;
+        this.stopFlag = stopFlag;
+        this.countDownLatch = new CountDownLatch(threadCount);
+
+        AtomicInteger nextId = new AtomicInteger(1);
+        ThreadFactory threadFactory = task -> {
+            String name = "exporter-" + nextId.getAndIncrement();
+            Thread thread = new Thread(null, task, name);
+            LOG.info(thread.getName() + " created");
+            return thread;
+        };
+        this.pool = new ThreadPoolExecutor(
+                threadCount,
+                threadCount,
+                0L,
+                TimeUnit.MILLISECONDS,
+                new LinkedBlockingQueue<>(),
+                threadFactory);
+        this.stopFn = stopFn;
+    }
+
+    public void start() {
+        for (int i = 0; i < threadCount; i ++) {
+            pool.execute(new ExportThread(exportQueue, restoreQueue, stopFlag, stopFn, countDownLatch));
+        }
+    }
+
+    public void stop() {
+        exportQueue.clear();
+        restoreQueue.clear();
+        pool.shutdownNow();
+    }
+
+    public void waitForComplete() {
+        try {
+            countDownLatch.await();
+            restoreQueue.put("");
+        } catch (InterruptedException e) {
+            LOG.error(e.getMessage(), e);
+        }
+        pool.shutdown();
+    }
+}
+
+class RestoreThread implements Runnable {
+    private static final Logger LOG = LoggerFactory.getLogger(RestoreThread.class);
+
+    private final BlockingQueue<String> restoreQueue;
+    private final AtomicBoolean stopFlag;
+    private final Consumer<Boolean> stopFn;
+//    private final JobStatusCounter statusCounter;
+//    private final RuleState ruleState;
+//    private final RuleConfig ruleConfig;
+
+    public RestoreThread(BlockingQueue<String> restoreQueue,
+                         AtomicBoolean stopFlag,
+                         Consumer<Boolean> stopFn) {
+        this.restoreQueue = restoreQueue;
+        this.stopFlag = stopFlag;
+        this.stopFn = stopFn;
+    }
+
+    private void restore(String snapshot) throws InterruptedException {
+        Thread.sleep(100);
+        LOG.info("restore snapshot: " + snapshot);
+    }
+
+    private boolean checkStopFlag() {
+        return stopFlag.get() || Thread.currentThread().isInterrupted();
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!checkStopFlag()) {
+                String snapshot = restoreQueue.take();
+                if ("".equals(snapshot)) {
+                    break;
+                }
+                restore(snapshot);
+            }
+        } catch (Exception e) {
+            if (!stopFlag.get()) {
+                LOG.error("unexpected exception in restore thread");
+                stopFn.accept(false);
+            }
+        } finally {
+            LOG.info("restore thread exited");
+        }
+    }
+}
+
+public class Backup {
+    private static final Logger LOG = LoggerFactory.getLogger(Backup.class);
+
+    private final int threadCount = 3;
+    private final BlockingQueue<String> snapshotQueue = new LinkedBlockingQueue<>();
+    private final BlockingQueue<String> exportQueue = new LinkedBlockingQueue<>();
+    private final BlockingQueue<String> restoreQueue = new LinkedBlockingQueue<>();
+    private AtomicBoolean stopFlag = new AtomicBoolean(false);
+    private volatile ExportThreadManager exportMgr;
+
+
+    public Backup() {
+        LOG.info("Backup constructor");
+    }
+
+    public void stop(Boolean userCancelled) {
+        stopFlag.set(true);
+        if (exportMgr != null) {
+            exportMgr.stop();
+        }
+        exportMgr = null;
+    }
+
+    public void start() {
+        List<String> tables = Arrays.asList("table1", "table2", "table3", "table4", "table5", "table6", "table7");
+        for (String table: tables) {
+            try {
+                snapshotQueue.put(table);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        this.exportMgr = new ExportThreadManager(threadCount, exportQueue, restoreQueue, stopFlag, this::stop);
+        exportMgr.start();
+        SnapshotThread snapshot = new SnapshotThread(threadCount, snapshotQueue, exportQueue, stopFlag, this::stop);
+        new Thread(snapshot, "generator").start();
+        RestoreThread restoreThread = new RestoreThread(restoreQueue, stopFlag, this::stop);
+        new Thread(restoreThread, "restorer").start();
+        exportMgr.waitForComplete();
+    }
+
+    public static void main(String[] args) {
+        Backup backup = new Backup();
+        backup.start();
+        backup.stop(true);
+    }
+}
+
