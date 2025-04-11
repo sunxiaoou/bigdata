3c8cc0ccbfc28f0c87c75510876698d0045c882e
sunxiaoou <sun_xiaoou@qq.com>
2025-04-11 16:30:37 +0800

    use distcpSnapshot instead of exportSnapshot

--- src/main/java/xo/hbase/ExportSnapshotDirect.java
+++ src/main/java/xo/hbase/ExportSnapshotDirect.java
@@ -1,6 +1,7 @@
 package xo.hbase;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hbase.*;
 import org.apache.hadoop.hbase.client.Admin;
 import org.apache.hadoop.hbase.client.Connection;
@@ -9,17 +10,19 @@ import org.apache.hadoop.hbase.client.SnapshotDescription;
 import org.apache.hadoop.hbase.security.User;
 import org.apache.hadoop.hbase.security.provider.SaslClientAuthenticationProvider;
 import org.apache.hadoop.hbase.security.provider.SaslClientAuthenticationProviders;
-import org.apache.hadoop.hbase.snapshot.ExportSnapshot;
 import org.apache.hadoop.hbase.util.Pair;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.security.UserGroupInformation;
 import org.apache.hadoop.security.token.Token;
 import org.apache.hadoop.security.token.TokenIdentifier;
-import org.apache.hadoop.util.ToolRunner;
+import org.apache.hadoop.tools.DistCp;
+import org.apache.hadoop.tools.DistCpOptions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.security.PrivilegedExceptionAction;
+import java.util.Collections;
 import java.util.List;
 
 public class ExportSnapshotDirect {
@@ -37,25 +40,6 @@ public class ExportSnapshotDirect {
         return provider.getFirst().getClass().getSimpleName();
     }
 
-    public static String createSnapshot(String confDir, String snapshotName, TableName tableName) throws IOException {
-        Configuration srcConf = HBaseConfiguration.create();
-        srcConf.addResource(confDir + "/core-site.xml");
-        srcConf.addResource(confDir + "/hdfs-site.xml");
-        srcConf.addResource(confDir + "/hbase-site.xml");
-
-        try (Connection srcConn = ConnectionFactory.createConnection(srcConf);
-             Admin admin = srcConn.getAdmin()) {
-            if (snapshotExists(snapshotName, admin)) {
-                LOG.warn("Snapshot {} already exists in source cluster, deleting...", snapshotName);
-                admin.deleteSnapshot(snapshotName);
-            }
-            admin.snapshot(snapshotName, tableName);
-            LOG.info("Snapshot '{}' created for table '{}'", snapshotName, tableName);
-        }
-//        LOG.info("Authentication provider '{}'", getProviderName(srcConf));
-        return srcConf.get("fs.defaultFS") + "/hbase";
-    }
-
     public static Configuration login(String confDir, String principal, String keytab) throws Exception {
         Configuration conf = HBaseConfiguration.create();
         conf.addResource(confDir + "/core-site.xml");
@@ -65,31 +49,60 @@ public class ExportSnapshotDirect {
         conf.addResource(confDir + "/hbase-site.xml");
 
         conf.setBoolean("ipc.client.fallback-to-simple-auth-allowed", true);
-        conf.setBoolean("dfs.client.use.legacy.blockreader", true);
-        conf.setBoolean("dfs.client.read.shortcircuit", false);
+        conf.set("mapreduce.map.memory.mb", "1536");
+        conf.set("mapred.child.java.opts", "-Xmx1024m");
 
         UserGroupInformation.reset();
         UserGroupInformation.setConfiguration(conf);
         UserGroupInformation.loginUserFromKeytab(principal, keytab);
-        LOG.info("Logged in as '{}' with '{}'", UserGroupInformation.getLoginUser(), getProviderName(conf));
+        LOG.info("Logged in as '{}'", UserGroupInformation.getLoginUser());
+        String provider = getProviderName(conf);
+        if (!"GssSaslClientAuthenticationProvider".equals(provider)) {
+            throw new RuntimeException("Unsupported authentication provider: " + provider);
+        }
         return conf;
     }
 
-    public static int exportSnapshot(Configuration conf, String snapshotName, String copyFrom)
-            throws Exception {
-        String[] exportArgs = new String[]{
-                "--snapshot", snapshotName,
-                "--copy-from", copyFrom,
-                "--copy-to", conf.get("fs.defaultFS") + "/hbase",
-                "--overwrite"
-        };
+    public static String createSnapshot(String confDir, String snapshotName, TableName tableName) throws IOException {
+        Configuration conf = HBaseConfiguration.create();
+        conf.addResource(confDir + "/core-site.xml");
+        conf.addResource(confDir + "/hdfs-site.xml");
+        conf.addResource(confDir + "/hbase-site.xml");
 
-        int result = ToolRunner.run(conf, new ExportSnapshot(), exportArgs);
-        if (result != 0) {
-            LOG.error("ExportSnapshot failed with rc={}", result);
-            throw new IOException("ExportSnapshot failed with rc=" + result);
+        try (Connection srcConn = ConnectionFactory.createConnection(conf);
+             Admin admin = srcConn.getAdmin()) {
+            if (snapshotExists(snapshotName, admin)) {
+                LOG.warn("Snapshot {} already exists in source cluster, deleting...", snapshotName);
+                admin.deleteSnapshot(snapshotName);
+            }
+            admin.snapshot(snapshotName, tableName);
+            LOG.info("Snapshot '{}' created for table '{}'", snapshotName, tableName);
         }
-        return result;
+        return conf.get("hbase.rootdir");
+    }
+
+    public static void distcpSnapshot(Configuration conf, String snapshotName, String copyFrom)
+            throws Exception {
+        String copyTo = conf.get("hbase.rootdir");
+        Path sourcePath = new Path(copyFrom + "/.hbase-snapshot/" + snapshotName);
+        Path targetPath = new Path(copyTo + "/.hbase-snapshot/" + snapshotName);
+        LOG.info("Copying snapshot from {} to {}", sourcePath, targetPath);
+
+        List<Path> srcPaths = Collections.singletonList(sourcePath);
+        DistCpOptions options = new DistCpOptions(srcPaths, targetPath);
+        options.setSyncFolder(true);
+        options.setDeleteMissing(true);
+
+        DistCp distCp = new DistCp(conf, options);
+        Job job = distCp.execute();
+        if (job.isSuccessful()) {
+            LOG.info("DistCp job {} completed successfully", job.getJobID());
+            LOG.info("Tracking URL: {}", job.getTrackingURL());
+        } else {
+            LOG.error("DistCp job {} failed", job.getJobID());
+            LOG.error("Reason: {}", job.getStatus().getFailureInfo());
+            throw new RuntimeException("DistCp failed");
+        }
     }
 
     public static void cloneSnapshot(Configuration conf, String snapshotName, TableName tableName)
@@ -125,8 +138,7 @@ public class ExportSnapshotDirect {
 
         Configuration tgtConf = login(targetConfDir, principal, keytab);
         String copyFrom = createSnapshot(sourceConfDir, snapshotName, tableName);
-        if (0 == exportSnapshot(tgtConf, snapshotName, copyFrom)) {
-            cloneSnapshot(tgtConf, snapshotName, tableName);
-        }
+        distcpSnapshot(tgtConf, snapshotName, copyFrom);
+        cloneSnapshot(tgtConf, snapshotName, tableName);
     }
 }
