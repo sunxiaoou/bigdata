0d681e30523471a0f1946bc45b041d51942c4cca
sunxiaoou <sun_xiaoou@qq.com>
2025-11-26 10:09:16 +0800

    migrate to mac M4

--- src/main/java/xo/hbase/ExportSnapshotTest.java
+++ src/main/java/xo/hbase/ExportSnapshotTest.java
@@ -1,11 +1,15 @@
 package xo.hbase;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.security.UserGroupInformation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExportSnapshotDirect {
-    private static final Logger LOG = LoggerFactory.getLogger(ExportSnapshotDirect.class);
+import java.io.IOException;
+import java.security.PrivilegedExceptionAction;
+
+public class ExportSnapshotTest {
+    private static final Logger LOG = LoggerFactory.getLogger(ExportSnapshotTest.class);
 
     public static void main(String[] args) throws Exception {
         String snapshot = "manga-fruit_snapshot";
@@ -19,15 +23,29 @@ public class ExportSnapshotDirect {
         Configuration conf = HBase.loadConf(tgtPath, zPrincipal, false);
         HBase.login(conf, principal, keytab);
 
-        HBase srcDb = new HBase(srcPath, null, null, null, false);
-        String copyFrom = srcDb.getProperty("hbase.rootdir");
-        srcDb.createSnapshot(table, snapshot);
-        srcDb.close();
-        conf = HBase.loadConf(tgtPath, zPrincipal, true);
-        String copyTo = conf.get("hbase.rootdir");
-        HBase.exportSnapshot(conf, snapshot, copyFrom, copyTo);
-        HBase tgtDb = new HBase(tgtPath, zPrincipal, false);
-        tgtDb.cloneSnapshot(snapshot, table);
-        tgtDb.close();
+//        UserGroupInformation ugi = UserGroupInformation.getLoginUser();
+//        ugi.doAs((PrivilegedExceptionAction<Void>) () -> {
+//                    HBase srcDb = new HBase(srcPath, null, null, null, false);
+//                    String copyFrom = srcDb.getProperty("hbase.rootdir");
+//                    srcDb.createSnapshot(table, snapshot);
+//                    srcDb.close();
+//                    return null;
+//                });
+//        String copyFrom = "hdfs://ubuntu:8020/hbase";
+//        conf = HBase.loadConf(tgtPath, zPrincipal, true);
+//        String copyTo = conf.get("hbase.rootdir");
+//        HBase.exportSnapshot(conf, snapshot, copyFrom, copyTo);
+        Thread.sleep(2000);
+
+        Thread thread = new Thread(() -> {
+            try {
+                HBase tgtDb = new HBase(tgtPath, zPrincipal, false);
+                tgtDb.cloneSnapshot(snapshot, table);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        thread.start();
+        thread.join();
     }
 }
